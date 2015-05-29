efe260d18fe0fc8b388893f171e3d3b1a47b1eaa
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-05-28 23:18:46 -0400

    Create sorting options for movie index, crude pagination for movies and actors.

--- server.rb
+++ server.rb
@@ -16,48 +16,76 @@ get '/' do
 end
 
 get '/movies' do
-  movies_table = db_connection{ |conn| conn.exec(
-    "SELECT movies.title, movies.year, movies.rating, genres.name AS genre, studios.name AS studio_name
-      FROM movies
-      LEFT JOIN genres ON movies.genre_id = genres.id
-      RIGHT JOIN studios ON movies.studio_id = studios.id
-      ORDER BY title ASC
-      LIMIT 10")}
+
+  order = params['order']
+  where = ""
+  if order == nil || order == "title"
+    order = "title"
+    sort = "ASC"
+  elsif order == "rating"
+    sort = "DESC"
+    where = "WHERE rating IS NOT NULL"
+  else
+    sort = "DESC"
+  end
+
+  page = params['page']
+  if page == nil
+    offset = 0
+  else
+    offset = (page.to_i - 1) * 20
+  end
+
+  sql= "SELECT movies.title, movies.id, movies.year, movies.rating,
+         genres.name AS genre, studios.name AS studio_name
+        FROM movies
+        LEFT JOIN genres ON movies.genre_id = genres.id
+        RIGHT JOIN studios ON movies.studio_id = studios.id
+        #{where}
+        ORDER BY movies.#{order} #{sort}
+        LIMIT 20
+        OFFSET #{offset}"
+  movies_table = db_connection{ |conn| conn.exec(sql)}
   movies = movies_table.to_a
-  erb :'/movies/index', locals: { movies: movies }
+
+  erb :'/movies/index', locals: { movies: movies, page: page, order: order }
 end
 
 get '/movies/:id' do
-  movie_param_id = [params["id"]]
-  movie_table = db_connection{ |conn| conn.exec(
-    "SELECT movies.title AS movie_title,
-    genres.name AS genre,
-    studios.name AS studio,
-    cast_members.character AS role,
-    actors.name AS actor,
-    actors.id AS actor_id
-    FROM movies
-    JOIN genres ON genres.id = movies.genre_id
-    JOIN studios ON studios.id = movies.studio_id
-    RIGHT JOIN cast_members ON movies.id = cast_members.movie_id
-    RIGHT JOIN actors ON cast_members.actor_id = actors.id
-    WHERE movies.id = $1
-    ORDER BY cast_members.character
-    LIMIT 20", movie_param_id
-    )};
+  sql= "SELECT movies.title AS movie_title,
+  genres.name AS genre,
+  studios.name AS studio,
+  cast_members.character AS role,
+  actors.name AS actor,
+  actors.id AS actor_id
+  FROM movies
+  JOIN genres ON genres.id = movies.genre_id
+  JOIN studios ON studios.id = movies.studio_id
+  RIGHT JOIN cast_members ON movies.id = cast_members.movie_id
+  RIGHT JOIN actors ON cast_members.actor_id = actors.id
+  WHERE movies.id = #{params['id']}
+  ORDER BY cast_members.character"
+  movie_table = db_connection{ |conn| conn.exec(sql)};
   movie = movie_table.to_a
   erb :'/movies/show', locals: { movie: movie, id: params[:id] }
 end
 
 get '/actors' do
-  actors_table = db_connection{ |conn| conn.exec(
-    "SELECT actors.id, actors.name
-    FROM actors
-    ORDER BY name
-    LIMIT 10"
-    )}
+  page = params['page']
+    if page == nil
+      offset = 0
+    else
+      offset = (page.to_i - 1) * 20
+    end
+
+  sql = "SELECT actors.id, actors.name
+  FROM actors
+  ORDER BY name
+  LIMIT 20
+  OFFSET #{offset}"
+  actors_table = db_connection{ |conn| conn.exec(sql)}
     actors = actors_table.to_a
-  erb :'actors/index', locals: { actors: actors }
+  erb :'actors/index', locals: { actors: actors, page: page }
 end
 
 get '/actors/:id' do
